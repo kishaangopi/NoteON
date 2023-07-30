90e5c2a1e4c1123402e657e8b0b2c54e0b2aa447
P Gopikishaan <kishaangopi@gmail.com>
2023-07-30 22:48:07 +0530

    backend url

--- frontend/src/features/apiSlice.js
+++ frontend/src/features/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-	baseUrl: "http://localhost:9000/",
+	baseUrl: process.env.REACT_APP_URL,
 	credentials: "include",
 });
 
