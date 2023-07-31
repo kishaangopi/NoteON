941e42fddad228b3f58672f026acfc8679724135
P Gopikishaan <kishaangopi@gmail.com>
2023-07-31 09:14:07 +0530

    http only updated

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -5,7 +5,7 @@ const generateToken = (res, id) => {
 	const token = jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
 
 	res.cookie("jwt", token, {
-		httpOnly: true,
+		httpOnly: false,
 		secure: process.env.NODE_ENV !== "development",
 		sameSite: "strict",
 		maxAge: 30 * 24 * 60 * 60 * 1000,
