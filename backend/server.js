0c477616c660b76b15f99404e9ec1e04721efafe
P Gopikishaan <kishaangopi@gmail.com>
2023-07-31 19:39:35 +0530

    middleware updated

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
-app.use(middleware);
 
 app.use(express.static(path.join(__dirname, "public")));
 
