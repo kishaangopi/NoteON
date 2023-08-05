da5fffa9e72a152525d256393a62e5193f836d4f
P Gopikishaan <kishaangopi@gmail.com>
2023-08-05 14:00:18 +0530

    changing domains

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,7 +1,6 @@
 const allowedOrigins = [
 	"http://localhost:3000",
-	"https://note-on-phi.vercel.app",
-	"https://note-on-seven.vercel.app/",
+	"https://note-on-seven.vercel.app",
 ];
 
 module.exports = allowedOrigins;
