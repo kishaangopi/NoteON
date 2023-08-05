83438dfaacf271e6bc68d64eb2700cbcbc407f05
P Gopikishaan <kishaangopi@gmail.com>
2023-08-05 13:13:21 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -22,9 +22,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", require("./routes/root"));
-app.get("/", (req, res) => {
-	res.send("Hello World");
-});
 app.use("/notes", require("./routes/noteRoutes"));
 app.use("/users", require("./routes/userRoutes"));
 
